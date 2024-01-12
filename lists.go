1ba1bb2077de0a7a6afe2d2274c1e1065e6bf844
Gilles Faucherand <gilles.faucherand@unil.ch>
2024-01-12 09:28:19 +0100

    6.117

--- lists.go
+++ lists.go
@@ -9,31 +9,48 @@ type Product struct {
 }
 
 func main() {
-
-	prices := [5]float64{1.2, 3.4, 5.6, 7.8, 9.0}
+	prices := []float64{1.2, 3.4}
 
 	fmt.Println(prices)
 
-	var productNames [5]string = [5]string{"Pen", "Pencil", "Marker"}
-	productNames[3] = "Eraser"
+	updatedPrices := append(prices, 5.6)
+	fmt.Println(updatedPrices)
 
-	fmt.Println(productNames)
-	fmt.Println(productNames[0])
-	fmt.Println(productNames[1:3])
-	fmt.Println(productNames[:2])
-	fmt.Println(productNames[2:])
+	prices[0] = 0.0
+	fmt.Println(prices, updatedPrices)
 
-	// pointer
-	featuredProduct := productNames[:2]
-	fmt.Println(featuredProduct)
-	featuredProduct[0] = "NewPen"
-	fmt.Println(productNames)
-	fmt.Println(featuredProduct)
+	discountPrices := []float64{0.5, 0.6, 0.7}
+	prices = append(prices, discountPrices...)
+	fmt.Println(prices, discountPrices)
 
-	fmt.Println(len(productNames), cap(productNames))
-	fmt.Println(len(featuredProduct), cap(featuredProduct))
-
-	featuredProduct = featuredProduct[:cap(featuredProduct)]
-	fmt.Println(featuredProduct)
-	fmt.Println(len(featuredProduct), cap(featuredProduct))
 }
+
+// func main() {
+
+// 	prices := [5]float64{1.2, 3.4, 5.6, 7.8, 9.0}
+
+// 	fmt.Println(prices)
+
+// 	var productNames [5]string = [5]string{"Pen", "Pencil", "Marker"}
+// 	productNames[3] = "Eraser"
+
+// 	fmt.Println(productNames)
+// 	fmt.Println(productNames[0])
+// 	fmt.Println(productNames[1:3])
+// 	fmt.Println(productNames[:2])
+// 	fmt.Println(productNames[2:])
+
+// 	// pointer
+// 	featuredProduct := productNames[:2]
+// 	fmt.Println(featuredProduct)
+// 	featuredProduct[0] = "NewPen"
+// 	fmt.Println(productNames)
+// 	fmt.Println(featuredProduct)
+
+// 	fmt.Println(len(productNames), cap(productNames))
+// 	fmt.Println(len(featuredProduct), cap(featuredProduct))
+
+// 	featuredProduct = featuredProduct[:cap(featuredProduct)]
+// 	fmt.Println(featuredProduct)
+// 	fmt.Println(len(featuredProduct), cap(featuredProduct))
+// }
