26b8e281c21271a1cc77c5f62e5bb041afe38c1c
Gilles Faucherand <gilles.faucherand@unil.ch>
2024-01-15 09:56:42 +0100

    8.126

--- main.go
+++ main.go
@@ -2,43 +2,30 @@ package main
 
 import "fmt"
 
-type floatMap map[string]float64
-
-func (m floatMap) output() {
-	fmt.Println(m)
-}
+type transformFn func(int) int
 
 func main() {
 
-	// usrName := []string{}
-	//
-	usrName := make([]string, 2, 3)
+	numbers := []int{1, 2, 3, 4}
+	fmt.Println(numbers)
+	doubled := tranformNumbers(&numbers, double)
+	fmt.Println(doubled)
+	tripled := tranformNumbers(&numbers, triple)
+	fmt.Println(tripled)
+}
 
-	usrName[0] = "John1"
-	fmt.Println(usrName)
-
-	usrName = append(usrName, "John2")
-	usrName = append(usrName, "Doe")
-
-	fmt.Println(usrName)
-
-	// courseRating := map[string]float64{}
-	// courseRating := make(map[string]float64, 2)
-	courseRating := make(floatMap, 2)
-
-	courseRating["GoLang"] = 5
-	courseRating["Python"] = 4.5
-	courseRating["Java"] = 4.5
-
-	// fmt.Println(courseRating)
-	courseRating.output()
-
-	for index, value := range usrName {
-		fmt.Println(index, value)
+func tranformNumbers(numbers *[]int, transform transformFn) []int {
+	doubleNumbers := []int{}
+	for _, v := range *numbers {
+		doubleNumbers = append(doubleNumbers, transform(v))
 	}
+	return doubleNumbers
+}
 
-	for key, value := range courseRating {
-		fmt.Println(key, value)
-	}
+func double(number int) int {
+	return number * 2
+}
 
+func triple(number int) int {
+	return number * 3
 }
