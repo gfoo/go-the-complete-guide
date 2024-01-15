6b6f2a0911fd0c10dba18d36af08bf35b483fcf4
Gilles Faucherand <gilles.faucherand@unil.ch>
2024-01-15 13:56:25 +0100

    8.129

--- main.go
+++ main.go
@@ -2,48 +2,35 @@ package main
 
 import "fmt"
 
-type transformFn func(int) int
-
 func main() {
+	numbers := []int{1, 2, 3}
 
-	numbers := []int{1, 2, 3, 4}
-	fmt.Println(numbers)
+	transformed := transformNumbers(&numbers, func(val int) int {
+		return val * 2
+	})
+	fmt.Println(transformed)
+
+	double := createTrans(2)
+	triple := createTrans(3)
+
 	doubled := transformNumbers(&numbers, double)
 	fmt.Println(doubled)
 	tripled := transformNumbers(&numbers, triple)
 	fmt.Println(tripled)
-
-	moreNumbers := []int{5, 1, 2}
-
-	transformFn1 := getTransformFn(&numbers)
-	transformFn2 := getTransformFn(&moreNumbers)
-
-	transformedNumbers := transformNumbers(&numbers, transformFn1)
-	moreTransformedNumbers := transformNumbers(&moreNumbers, transformFn2)
-
-	fmt.Println(transformedNumbers)
-	fmt.Println(moreTransformedNumbers)
 }
 
-func getTransformFn(numbers *[]int) transformFn {
-	if (*numbers)[0] == 1 {
-		return double
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
+	dNumbers := []int{}
+
+	for _, val := range *numbers {
+		dNumbers = append(dNumbers, transform(val))
 	}
-	return triple
+
+	return dNumbers
 }
 
-func transformNumbers(numbers *[]int, transform transformFn) []int {
-	doubleNumbers := []int{}
-	for _, v := range *numbers {
-		doubleNumbers = append(doubleNumbers, transform(v))
+func createTrans(factor int) func(int) int {
+	return func(val int) int {
+		return val * factor
 	}
-	return doubleNumbers
-}
-
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
 }
