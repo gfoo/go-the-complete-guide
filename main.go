d2335c054f0121aca9a5b4da07921479353b2a4c
Gilles Faucherand <gilles.faucherand@unil.ch>
2024-01-16 11:03:47 +0100

    8.132

--- main.go
+++ main.go
@@ -4,12 +4,27 @@ import "fmt"
 
 func main() {
 
-	fmt.Println("Factorial of 5 is: ", factorial(5))
+	numbers := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(sumup(numbers))
+
+	fmt.Println(sumup2(1, 2, 3, 4, 5))
+
+	fmt.Println(sumup2(numbers...))
 }
 
-func factorial(n int) int {
-	if n == 0 {
-		return 1
+func sumup(numbers []int) int {
+	sum := 0
+	for _, val := range numbers {
+		sum += val
 	}
-	return n * factorial(n-1)
+	return sum
+}
+
+func sumup2(numbers ...int) int {
+	sum := 0
+	for _, val := range numbers {
+		sum += val
+	}
+	return sum
 }
