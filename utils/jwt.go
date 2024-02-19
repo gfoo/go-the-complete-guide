2b5230aeb97b797b62e4da2ecd500896abd61407
Gilles Faucherand <gilles.faucherand@unil.ch>
2024-02-19 11:23:13 +0100

    11.187

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"errors"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -15,4 +16,33 @@ func GenerateToken(email string, userId int64) (string, error) {
 			"userId": userId,
 			"exp":    time.Now().Add(time.Hour * 2).Unix(),
 		}).SignedString([]byte(secretKey))
+}
+
+func VerifyToken(token string) (int64, error) {
+	parsedToken, err := jwt.Parse(
+		token,
+		func(token *jwt.Token) (interface{}, error) {
+			_, ok := token.Method.(*jwt.SigningMethodHMAC)
+			if !ok {
+				return nil, errors.New("Unexpected signing method")
+			}
+			return []byte(secretKey), nil
+		},
+	)
+	if err != nil {
+		return 0, errors.New("Could not parse token")
+	}
+
+	if !parsedToken.Valid {
+		return 0, errors.New("Invalid token")
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return 0, errors.New("Invalid token claims")
+	}
+	// email := claims["email"].(string)
+	userId := int64(claims["userId"].(float64))
+
+	return userId, nil
 }
